8e11e8c02339ef6b4b6da57774d64ec8a7b1277b
Ayaz <ayazumman@gmail.com>
2024-01-30 00:28:00 +0400

    dfdf

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from "./app";
 
 const start = async () => {
   console.log("Ssdsdjdssss");
-  console.log("dkkfg");
+  console.log("dkkfgmmc,");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
